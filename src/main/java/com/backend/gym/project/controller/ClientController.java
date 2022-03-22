2c648ea363976e532028009a2cc450064be597c8
Victor Torres <torresvictor100@gmail.com>
2022-03-22 14:09:32 -0300

    camada de controller em andamento, com o crud simples pronto faltando colocar as funções especificas

---
+++ src/main/java/com/backend/gym/project/controller/ClientController.java
@@ -0,0 +1,112 @@
+package com.backend.gym.project.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.gym.project.entity.Client;
+import com.backend.gym.project.service.ClientService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+@RequestMapping(path = "/client")
+public class ClientController {
+	
+	private final ClientService clientService;
+
+	public ClientController(ClientService clientService) {
+		super();
+		this.clientService = clientService;
+	}
+	
+	@ApiOperation(value = "Find all client")
+	@ApiResponses({ @ApiResponse(code = 200, message = "OK") })
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<List<Client>> findAll(@RequestParam(name = "name", required = false) String name) {
+		List<Client> client = clientService.findAll();
+		return new ResponseEntity<>(client, HttpStatus.OK);
+	}
+	
+	@ApiOperation(value = "Find a client by id")
+	@ApiResponses({ @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 400, message = "Bad Request"),
+			@ApiResponse(code = 404, message = "Not Found") })
+	@GetMapping(path = "/{client_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Client> findById(@PathVariable(name = "client_id") Long id) {
+		Client client = clientService.findById(id);
+		client = clientService.monthlyFee(id);
+		if (client == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(client, HttpStatus.OK);
+		}
+	}
+	
+	@ApiOperation(value = "Save a client")
+	@ApiResponses({ @ApiResponse(code = 201, message = "Created"), @ApiResponse(code = 400, message = "Bad Request") })
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Client> save(@RequestBody Client client) {
+		try {
+			client = clientService.save(client);
+			return new ResponseEntity<>(client, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@ApiOperation(value = "Update a client")
+	@ApiResponses({ @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 400, message = "Bad Request"),
+			@ApiResponse(code = 404, message = "Not Found") })
+	@PutMapping(path = "/{client_id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Client> update(@PathVariable(name = "client_id") Long id,
+			@RequestBody Client client) {
+		client.setId(id);
+		try {
+			client = clientService.update(client);
+			if (client == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			} else {
+				return new ResponseEntity<>(client, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@ApiOperation(value = "Delete a client")
+	@ApiResponses({ @ApiResponse(code = 204, message = "No Content") })
+	@DeleteMapping(path = "/{client_id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Void> deleteById(@PathVariable(name = "client_id") Long id) {
+		clientService.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@ApiOperation(value = "Delete all cashdesk")
+	@ApiResponses({ @ApiResponse(code = 204, message = "No Content") })
+	@DeleteMapping
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<Void> deleteAll() {
+		clientService.deleteAll();
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+}
