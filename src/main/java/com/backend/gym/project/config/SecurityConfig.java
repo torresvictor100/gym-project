88fe026cb58a6260dc89dc274f8866573c025477
João Victor Torres <torresvictor100@gmail.com>
2022-04-16 12:10:02 -0300

    Implementando autorizacao

--- src/main/java/com/backend/gym/project/config/SecurityConfig.java
+++ src/main/java/com/backend/gym/project/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.backend.gym.project.security.JWTAuthenticationFilter;
+import com.backend.gym.project.security.JWTAuthorizationFilter;
 import com.backend.gym.project.security.JWTUtil;
 
 @Configuration
@@ -53,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
